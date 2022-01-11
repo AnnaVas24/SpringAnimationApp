95e0035c65656291b1719c23d77ed760befc29e3
Anna <anna.vasi4ko@icloud.com>
2022-01-11 18:08:18 +0400

    Initial Commit

---
+++ SpringAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationApp
+//
+//  Created by Vasichko Anna on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
